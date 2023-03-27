f1d1446e2a7f00c18f5136c4a94ecf98a5d92f07
Barry Chenault <barry.j.chenault@gmail.com>
2023-03-27 14:52:47 -0500

    updating database

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const image = require('./Controllers/image')
 const db = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
+      host : 'postgresql-rugged-88243',
       port : process.env.PORT,
       user : 'barrychenault',
       password : '',
