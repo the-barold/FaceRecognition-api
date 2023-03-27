fa6477dff6f0e8c1a6c40bac5cba22f9babe129b
Barry Chenault <barry.j.chenault@gmail.com>
2023-03-27 14:13:42 -0500

    reinstalled nodemon

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const db = knex({
     client: 'pg',
     connection: {
       host : '127.0.0.1',
-      //port : 5432,
+      port : process.env.PORT,
       user : 'barrychenault',
       password : '',
       database : 'smart-brain'
